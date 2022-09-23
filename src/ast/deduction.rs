74027598d8597e43ce210d912075b4b91d719dd3
Sawyer Bergeron <sawyerbergeron@gmail.com>
2022-09-23 16:42:40 -0400

    deduction work

---
+++ src/ast/deduction.rs
@@ -0,0 +1,9 @@
+/// Deduction method takes a Linear and resolves types by doing straightforward assignment back
+/// propagation
+///
+/// Constructors must be able to be fully constrained from forward stepping from their import,
+/// so the base of a call or expression must be something we can resolve directly to a module or item
+/// 
+/// 
+///
+
